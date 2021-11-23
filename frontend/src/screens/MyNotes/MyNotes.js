1aa468cfc4df9abedb8f28a1c61b6a7a3ceba2c8
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-11-23 12:35:26 +0600

    Error: Element type is invalid: expected a string (for built-in components) or a class/function (for composite components) but got: undefined. You likely forgot to export your component from the file it's defined in, or you might have mixed up default and named imports.

--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,7 +1,66 @@
 import MainScreen from "../../components/MainScreen";
-
+import { Button, Card, Badge, Accordion } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import notes from "../../data/notes";
 const MyNotes = () => {
-  return <MainScreen title="Welcome Back Jack">My Notes</MainScreen>;
+  const deleteHandler = (id) => {
+    if (window.confim("Are you Sure?")) {
+    }
+  };
+
+  return (
+    <MainScreen title="Welcome Back Jack">
+      <Link to="/createnote">
+        <Button style={{ marginLeft: 10, marginBottom: 6 }} size="lg">
+          Create New Note
+        </Button>
+      </Link>
+      {notes.map((note) => (
+        <Accordion>
+          <Card style={{ margin: 10 }} key={note._id}>
+            <Card.Header style={{ display: "flex" }}>
+              <span
+                // onClick={() => ModelShow(note)}
+                style={{
+                  color: "black",
+                  textDecoration: "none",
+                  flex: 1,
+                  cursor: "pointer",
+                  alignSelf: "center",
+                  fontSize: 18,
+                }}
+              >
+                <Accordion.Toggle as={Card.Text} variant="link" eventKey="0">
+                  {note.title}
+                </Accordion.Toggle>
+              </span>
+
+              <div>
+                <Button href={`/note/${note._id}`}>Edit</Button>
+                <Button
+                  variant="danger"
+                  className="mx-2"
+                  onClick={() => deleteHandler(note._id)}
+                >
+                  Delete
+                </Button>
+              </div>
+            </Card.Header>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>
+                <h4>
+                  <Badge variant="success">Category - {note.category}</Badge>
+                </h4>
+                <blockquote className="blockquote mb-0">
+                  Create on - Date
+                </blockquote>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+      ))}
+    </MainScreen>
+  );
 };
 
 export default MyNotes;
