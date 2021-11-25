bde9fdefae544f5922c0deeac4d40976dcf6e157
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-11-25 13:24:12 +0600

    Backend Login API

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser } = require("../controllers/userControllers");
+const { registerUser, authUser } = require("../controllers/userControllers");
 
 router.route("/").post(registerUser);
+router.route("/login").post(authUser);
 
 module.exports = router;
