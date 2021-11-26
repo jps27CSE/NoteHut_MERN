6939fb6ba6199bfeece0994c7b41e7e6d5900c08
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-11-26 21:42:21 +0600

    Registration Login

--- frontend/src/screens/RegisterScreen/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen/RegisterScreen.js
@@ -1,5 +1,124 @@
+import { Form, Button, Row, Col } from "react-bootstrap";
+import ErrorMessage from "../../components/ErrorMessage";
+import Loading from "../../components/Loading";
+import MainScreen from "../../components/MainScreen";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 const RegisterScreen = () => {
-  return <div>Register</div>;
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [pic, setPic] = useState(
+    "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png"
+  );
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState(null);
+  const [picMessage, setPicMessage] = useState(null);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(email);
+
+    if (password !== confirmPassword) {
+      setMessage("Password Do not Match");
+    } else {
+      setMessage(null);
+      try {
+        const config = {
+          headers: {
+            "Content-type": "application/json",
+          },
+        };
+
+        setLoading(true);
+
+        const { data } = await axios.post(
+          "/api/users",
+          { name, pic, email, password },
+          config
+        );
+        setLoading(false);
+        localStorage.setItem("userInfo", JSON.stringify(data));
+      } catch (error) {
+        setError(error.response.data.message);
+      }
+    }
+  };
+
+  return (
+    <MainScreen title="REGISTER">
+      <div className="loginContainer">
+        {error && <ErrorMessage variant="danger"></ErrorMessage>}
+        {message && <ErrorMessage variant="danger">{message}</ErrorMessage>}
+        {loading && <Loading />}
+
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="name"
+              value={name}
+              placeholder="Enter Name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email Adress</Form.Label>
+            <Form.Control
+              type="email"
+              value={email}
+              placeholder="Enter Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={password}
+              placeholder="Enter Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="confirmPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={confirmPassword}
+              placeholder="Confirm Password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </Form.Group>
+          {/* {{picMessage &&(<ErrorMessage variant="danger">{picMessage}</ErrorMessage>)}} */}
+
+          <Form.Group controlId="pic">
+            <Form.Label>Profile Picture</Form.Label>
+            <Form.File
+              id="custom-file"
+              type="image/png"
+              label="Upload Profile Picture"
+              custom
+              // onChange={(e)=>postDetails(e.target.files[0])}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Register
+          </Button>
+        </Form>
+        <Row className="py-3">
+          <Col>
+            Have an Account ? <Link to="/login">Login</Link>
+          </Col>
+        </Row>
+      </div>
+    </MainScreen>
+  );
 };
 
 export default RegisterScreen;
