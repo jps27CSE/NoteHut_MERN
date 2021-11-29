c2b36e1980717c2ce27427d985ec6b08eed43f76
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-11-30 00:22:22 +0600

    Added Crud Operation  API in Backend

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -2,8 +2,70 @@ const asyncHandler = require("express-async-handler");
 const Note = require("../models/noteModel");
 
 const getNotes = asyncHandler(async (req, res, next) => {
-  const notes = await Note.find();
+  const notes = await Note.find({ user: req.user._id });
   res.json(notes);
 });
 
-module.exports = { getNotes };
+const createNote = asyncHandler(async (req, res) => {
+  const { title, content, category } = req.body;
+
+  if (!title || !content || !category) {
+    throw new Error("Please Fill all the Fields");
+  } else {
+    const note = new Note({ user: req.user._id, title, content, category });
+
+    const createNote = await note.save();
+
+    res.status(201).json(createNote);
+  }
+});
+
+const getNoteById = asyncHandler(async (req, res) => {
+  const note = await Note.findById(req.params.id);
+
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).json({ message: "Note not found" });
+  }
+});
+
+const UpdateNote = asyncHandler(async (req, res) => {
+  const { title, content, category } = req.body;
+
+  const note = await Note.findById(req.params.id);
+
+  if (note.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error("You can perform this action");
+  }
+  if (note) {
+    note.title = title;
+    note.content = content;
+    note.category = category;
+
+    const updatedNote = await note.save();
+    res.json(updatedNote);
+  } else {
+    throw new Error("note not found");
+  }
+});
+
+const DeleteNote = asyncHandler(async (req, res) => {
+  const note = await Note.findById(req.params.id);
+
+  if (note.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error("You can't perform this action");
+  }
+
+  if (note) {
+    await note.remove();
+    res.json({ message: "Note Removed" });
+  } else {
+    res.status(404);
+    throw new Error("Note not found");
+  }
+});
+
+module.exports = { getNotes, createNote, getNoteById, UpdateNote, DeleteNote };
--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,10 +1,20 @@
 const express = require("express");
-const { getNotes } = require("../controllers/noteController");
+const {
+  getNotes,
+  createNote,
+  getNoteById,
+  UpdateNote,
+  DeleteNote,
+} = require("../controllers/noteController");
 const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.route("/").get(protect, getNotes);
-// router.route("/create").post();
-// router.route("/:id").get().put().delete();
+router.route("/create").post(protect, createNote);
+router
+  .route("/:id")
+  .get(getNoteById)
+  .put(protect, UpdateNote)
+  .delete(protect, DeleteNote);
 
 module.exports = router;
