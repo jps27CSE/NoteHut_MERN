895118dad1b656228aa70cb1d30cca23f1851b7e
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-11-29 12:57:57 +0600

    Calling action in registration screen

--- frontend/src/screens/RegisterScreen/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen/RegisterScreen.js
@@ -3,8 +3,12 @@ import ErrorMessage from "../../components/ErrorMessage";
 import Loading from "../../components/Loading";
 import MainScreen from "../../components/MainScreen";
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import axios from "axios";
+import { useEffect, useState } from "react";
+// import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { register } from "../../actions/userActions";
+import { useHistory } from "react-router-dom";
+
 const RegisterScreen = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
@@ -15,36 +19,55 @@ const RegisterScreen = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [message, setMessage] = useState(null);
   const [picMessage, setPicMessage] = useState(null);
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(false);
+  // const [loading, setLoading] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const userRegister = useSelector((state) => state.userRegister);
+  const { loading, error, userInfo } = userRegister;
+
+  const history = useHistory();
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push("/mynotes");
+    }
+  }, [history, userInfo]);
 
   const submitHandler = async (e) => {
     e.preventDefault();
-    console.log(email);
+    // console.log(email);
+
+    // if (password !== confirmPassword) {
+    //   setMessage("Password Do not Match");
+    // } else {
+    //   setMessage(null);
+    //   try {
+    //     const config = {
+    //       headers: {
+    //         "Content-type": "application/json",
+    //       },
+    //     };
+
+    //     setLoading(true);
+
+    //     const { data } = await axios.post(
+    //       "/api/users",
+    //       { name, pic, email, password },
+    //       config
+    //     );
+    //     setLoading(false);
+    //     localStorage.setItem("userInfo", JSON.stringify(data));
+    //   } catch (error) {
+    //     setError(error.response.data.message);
+    //   }
+    // }
 
     if (password !== confirmPassword) {
       setMessage("Password Do not Match");
     } else {
-      setMessage(null);
-      try {
-        const config = {
-          headers: {
-            "Content-type": "application/json",
-          },
-        };
-
-        setLoading(true);
-
-        const { data } = await axios.post(
-          "/api/users",
-          { name, pic, email, password },
-          config
-        );
-        setLoading(false);
-        localStorage.setItem("userInfo", JSON.stringify(data));
-      } catch (error) {
-        setError(error.response.data.message);
-      }
+      dispatch(register(name, email, password, pic));
     }
   };
 
