8c106f030f26d8cb66607d3de1accdb3a948c9ee
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-12-03 23:04:55 +0600

    fixing deploy file

--- Backend/server.js
+++ Backend/server.js
@@ -21,16 +21,17 @@ app.use(express.json());
 // });
 
 // .................deployment................
-__dirname = path.resolve();
+const __dirname = path.resolve();
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
 } else {
   app.get("/", (req, res) => {
-    res.send("API is Running");
+    res.send("API is running..");
   });
 }
 // .................deployment................
