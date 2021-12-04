818c74457a56b57ad21ea3fa5b8dd7e776250c1e
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-12-04 19:12:36 +0600

    fix

--- Backend/server.js
+++ Backend/server.js
@@ -37,7 +37,8 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
-// } else {
+}
+// else {
 //   app.get("/", (req, res) => {
 //     res.send("API is running..");
 //   });
